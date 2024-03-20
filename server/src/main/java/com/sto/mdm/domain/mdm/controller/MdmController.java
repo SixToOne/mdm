43e847ba3a8ca2aaefb6a93929a764407645656f
차재환 <barded1998@gmail.com>
2024-03-20 15:14:08 +0900

    Feat : ✨ 몇대몇 게시글 수정

--- server/src/main/java/com/sto/mdm/domain/mdm/controller/MdmController.java
+++ server/src/main/java/com/sto/mdm/domain/mdm/controller/MdmController.java
@@ -1,12 +1,19 @@
 package com.sto.mdm.domain.mdm.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.sto.mdm.domain.mdm.dto.MdmRequestDto;
+import com.sto.mdm.domain.mdm.dto.MdmUpdateRequestDto;
 import com.sto.mdm.domain.mdm.service.MdmService;
 import com.sto.mdm.global.response.BaseResponse;
 
@@ -20,8 +27,20 @@ public class MdmController {
 	private final MdmService mdmService;
 
 	@PostMapping
-	ResponseEntity<BaseResponse<String>> postMdm(@RequestBody MdmRequestDto mdmRequestDto) {
-		mdmService.createMdm(mdmRequestDto);
+	ResponseEntity<BaseResponse<String>> postMdm(
+		@RequestPart MdmRequestDto mdmRequestDto,
+		@RequestPart(required = false) MultipartFile image1,
+		@RequestPart(required = false) MultipartFile image2,
+		@RequestPart(required = false) List<MultipartFile> images
+	) {
+		mdmService.createMdm(mdmRequestDto, image1, image2, images);
+		return ResponseEntity.ok(new BaseResponse<>(200, "success", null));
+	}
+
+	@PatchMapping("/{mdmId}")
+	ResponseEntity<BaseResponse<String>> patchMdm(@PathVariable Long mdmId,
+		@RequestBody MdmUpdateRequestDto mdmUpdateRequestDto) {
+		mdmService.updateMdm(mdmId, mdmUpdateRequestDto);
 		return ResponseEntity.ok(new BaseResponse<>(200, "success", null));
 	}
 
--- server/src/main/java/com/sto/mdm/domain/mdm/service/MdmService.java
+++ server/src/main/java/com/sto/mdm/domain/mdm/service/MdmService.java
@@ -1,15 +1,24 @@
 package com.sto.mdm.domain.mdm.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.sto.mdm.domain.mdm.dto.MdmRequestDto;
+import com.sto.mdm.domain.mdm.dto.MdmUpdateRequestDto;
 import com.sto.mdm.domain.mdm.entity.Mdm;
+import com.sto.mdm.domain.mdm.entity.MdmImage;
 import com.sto.mdm.domain.mdm.entity.MdmTag;
+import com.sto.mdm.domain.mdm.repository.MdmImageRepository;
 import com.sto.mdm.domain.mdm.repository.MdmRepository;
 import com.sto.mdm.domain.mdm.repository.MdmTagRepository;
 import com.sto.mdm.domain.tag.entity.Tag;
 import com.sto.mdm.domain.tag.repository.TagRepository;
+import com.sto.mdm.global.response.BaseException;
+import com.sto.mdm.global.response.ErrorCode;
+import com.sto.mdm.global.util.S3Uploader;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,13 +30,28 @@ public class MdmService {
 	private final MdmRepository mdmRepository;
 	private final TagRepository tagRepository;
 	private final MdmTagRepository mdmTagRepository;
+	private final S3Uploader s3Uploader;
+	private final MdmImageRepository mdmImageRepository;
 
 	@Transactional
-	public void createMdm(MdmRequestDto mdmRequestDto) {
+	public void createMdm(MdmRequestDto mdmRequestDto, MultipartFile image1, MultipartFile image2,
+		List<MultipartFile> images) {
 
 		Mdm mdm = mdmRequestDto.toEntity();
 
+		String imageUrl1 = s3Uploader.saveFile(image1);
+		String imageUrl2 = s3Uploader.saveFile(image2);
+
+		mdm.setImages(imageUrl1, imageUrl2);
+
 		mdmRepository.save(mdm);
+
+		images.forEach(image -> {
+			String imageUrl = s3Uploader.saveFile(image);
+			mdmImageRepository.save(MdmImage.builder()
+				.mdm(mdm)
+				.image(imageUrl).build());
+		});
 
 		mdmRequestDto.tags().forEach(t -> {
 			Tag tag = tagRepository.findByName(t)
@@ -35,6 +59,16 @@ public class MdmService {
 			mdmTagRepository.save(MdmTag.builder().mdm(mdm).tag(tag).build());
 		});
 
+	}
+
+	@Transactional
+	public void updateMdm(Long mdmId, MdmUpdateRequestDto mdmUpdateRequestDto) {
+		mdmRepository.findById(mdmId)
+			.ifPresentOrElse(mdm -> {
+				mdm.update(mdmUpdateRequestDto);
+			}, () -> {
+				throw new BaseException(ErrorCode.MDM_NOT_FOUND);
+			});
 	}
 }
 
