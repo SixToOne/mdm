8e7cedeb4692cacc91774f6e4caf5b80689f11eb
Seongeuniii <kse8538@naver.com>
2024-03-20 15:38:20 +0900

    Design : 💄 글로벌 스타일 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '@/theme/GlobalStyle';
 import Home from '@/pages/Home';
 import MDM from '@/pages/MDM';
 import MainLayout from '@/components/layouts/MainLayout';
+import theme from '@/theme';
 
 function App() {
     return (
-        <MainLayout>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/mdm/:id" element={<MDM />} />
-            </Routes>
-        </MainLayout>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <MainLayout>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/mdm/:id" element={<MDM />} />
+                </Routes>
+            </MainLayout>
+        </ThemeProvider>
     );
 }
 
