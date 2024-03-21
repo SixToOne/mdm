82c6c29be2ab32abb9682feb37558741279a8130
정유경 <yyk834@naver.com>
2024-03-21 14:06:22 +0900

    Feat : ✨ 퀴즈 상세

---
+++ server/src/main/java/com/sto/mdm/domain/quiz/controller/QuizController.java
@@ -0,0 +1,31 @@
+package com.sto.mdm.domain.quiz.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sto.mdm.domain.quiz.dto.QuizDto;
+import com.sto.mdm.domain.quiz.service.QuizService;
+import com.sto.mdm.global.response.BaseResponse;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/quizs")
+@RestController
+@Slf4j
+public class QuizController {
+	private final QuizService quizService;
+
+	//퀴즈 상세
+	@GetMapping("/{quizId}")
+	ResponseEntity<BaseResponse<QuizDto>> getQuizDetail(@PathVariable int quizId) {
+		QuizDto quizDetail = quizService.getQuizDetail(quizId);
+		return ResponseEntity.ok(new BaseResponse<>(HttpStatus.OK.value(), "success", quizDetail));
+	}
+
+}
--- server/src/main/java/com/sto/mdm/domain/quiz/repository/QuizTagRepository.java
+++ server/src/main/java/com/sto/mdm/domain/quiz/repository/QuizTagRepository.java
@@ -1,5 +1,6 @@
 package com.sto.mdm.domain.quiz.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,7 @@ import com.sto.mdm.domain.quiz.entity.QuizTag;
 public interface QuizTagRepository extends JpaRepository<QuizTag, Long> {
 
 	Optional<QuizTag> findById(Long id);
+
+	List<QuizTag> findAllByQuizId(Long quizId);
 
 }
