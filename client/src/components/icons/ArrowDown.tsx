5080b227d950dce4cc6b66e892d3091dc5c49987
김성은 <kse8538@naver.com>
2024-03-25 17:15:05 +0900

    Feat : ✨ 몇대몇 게시글 댓글, 대댓글 컴포넌트

---
+++ client/src/components/icons/ArrowDown.tsx
@@ -0,0 +1,9 @@
+const ArrowDown = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="9" viewBox="0 0 5 4" fill="none">
+            <path d="M2.5 4L0.334936 0.25L4.66506 0.25L2.5 4Z" fill="#0064FF" />
+        </svg>
+    );
+};
+
+export default ArrowDown;
