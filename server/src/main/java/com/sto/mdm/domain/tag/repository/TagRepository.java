07d36437c01d0d1889458415382f4d90c6364713
정유경 <yyk834@naver.com>
2024-03-27 13:07:55 +0900

    Feat : ✨ 태그 검색 기능

--- server/src/main/java/com/sto/mdm/domain/tag/repository/TagRepository.java
+++ server/src/main/java/com/sto/mdm/domain/tag/repository/TagRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.sto.mdm.domain.tag.entity.Tag;
 
@@ -11,4 +13,7 @@ public interface TagRepository extends JpaRepository<Tag, Long> {
 	Optional<Tag> findByName(String name);
 
 	List<Tag> findAllByName(String keyword);
+
+	@Query("SELECT t.name FROM Tag t WHERE t.name LIKE :keyword%")
+	List<String> searchTag(@Param("keyword") String keyword);
 }
