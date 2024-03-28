13d70e2f3cced9ecbbbe6fb1ab25509e222a9d25
나호균 <nahk99@naver.com>
2024-03-28 13:16:20 +0900

    Fix : 🐞 인기 게시글 자료형 맞게 불러오도록 수정

--- server/src/main/java/com/sto/mdm/domain/mdm/repository/MdmRepository.java
+++ server/src/main/java/com/sto/mdm/domain/mdm/repository/MdmRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import com.sto.mdm.domain.mdm.entity.Mdm;
 
 public interface MdmRepository extends JpaRepository<Mdm, Long> {
-	@Query(value = "select * from Mdm where created_at >= DATE_SUB(NOW(), INTERVAL 30 DAY) order by vote desc limit 10",nativeQuery = true)
-	List<Mdm> findHotMdm();
+	@Query(value = "select mdm_id from mdm where created_at >= DATE_SUB(NOW(), INTERVAL 30 DAY) order by vote desc limit 10",nativeQuery = true)
+	List<Integer> findHotMdm();
 }
